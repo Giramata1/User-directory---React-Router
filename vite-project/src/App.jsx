5106926b093914e21bf477a3cba65f26d01a80ba
Giramata1 <amugenigiramata@gmail.com>
2025-05-05 16:08:21 +0200

    Create App.jsx

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users/:id" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
